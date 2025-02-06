1d311ab9797723d1bb1beab632e052ab50197009
gulu43 <145833927+gulu43@users.noreply.github.com>
2025-02-06 13:30:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,23 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
+	let obj = {};
+    for (let i = 0; i < str.length; i++ ) {
+        if (obj[str[i]] == undefined) {
+            obj[str[i]] = 1;
+        }else{
+            obj[str[i]]++;
+        }
+    }
+
+    for (const x in obj) {
+        if (obj[x] < 2) {
+            // console.log(x); 
+            return x
+        }else{
+            return null
+        }
+    }
+	
 }
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
