ce2d83c0e4af8355d928658f4e3ae2047039a994
gulu43 <145833927+gulu43@users.noreply.github.com>
2025-02-06 13:40:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,22 +1,21 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
-	let obj = {};
-    for (let i = 0; i < str.length; i++ ) {
-        if (obj[str[i]] == undefined) {
-            obj[str[i]] = 1;
-        }else{
-            obj[str[i]]++;
-        }
-    }
-
-    for (const x in obj) {
-        if (obj[x] == 1) {
+	  let obj = {};
+       for (let i = 0; i < str.length; i++ ) {
+           if (obj[str[i]] == undefined) {
+               obj[str[i]] = 1;
+           }else{
+               obj[str[i]]++;
+           }
+       }
+   
+       for (const x in obj) {
+           if (obj[x] == 1) {
             // console.log(x); 
-            return x
-        }else{
-            return null;
+            return x;
         }
-    }
+       }
+   return null;
 	
 }
 const input = prompt("Enter a string");
